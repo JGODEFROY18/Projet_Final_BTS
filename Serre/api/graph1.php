0dac2af0c21b450926efa0c6e71686e4d126ce02
ReneMichelDecuy <100191398+ReneMichelDecuy@users.noreply.github.com>
2023-05-23 17:24:28 +0200

    Update graph1.php

--- Serre/api/graph1.php
+++ Serre/api/graph1.php
@@ -1,17 +1,21 @@
+<?php 
+    $base = new PDO("mysql:host=192.168.64.189;dbname=BDDProjet", "ADMIN", "ADMIN");
+    $resultat = $base->query("Select * from data");
+    $donnees = $resultat->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['bar']});
       google.charts.setOnLoadCallback(drawChart);
-
+      var donnees = <?php echo json_encode($donnees);?>;
+      console.log(donnees);
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['Date', 'Conso Electrique', 'Conso Eau', 'Capt Humidité Intérieur', 'Capt Température Intérieur','Capt Humidité Bac 1','Capt Humidité Bac 2','Capt Humidité Bac 3'],
-          ['2014', 1000, 400, 200, 5, 6, 7, 8],
-          ['2015', 1170, 460, 250, 5, 6, 7, 8],
-          ['2016', 660, 1120, 300, 5, 6, 7, 8],
-          ['2017', 1030, 540, 350, 5, 6, 7, 8]
+          ['Date', 'Conso Electrique', 'Conso Eau', 'Capt Humidité Intérieur', 'Capt Température Intérieur' ,'Capt Température Exterieur' ,'Capt Humidité Bac 1','Capt Humidité Bac 2','Capt Humidité Bac 3'],
+         [donnees[0].date, donnees[0].consoElec, donnees[0].consoEau, donnees[0].capHumInt, donnees[0].capTempInt, donnees[0].capTempExt, donnees[0].capHumBac1, donnees[0].capHumBac2, donnees[0].capHumBac3]
         ]);
 
         var options = {
